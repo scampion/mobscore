a99e07104b1065b2b03af98145ecb227402694cf
Sébastien Campion <sebastien.campion@foss4.eu>
2022-10-09 10:27:21 +0200

    update

--- telraam.py
+++ telraam.py
@@ -49,29 +49,36 @@ def traffic_last_3_months(segment_id):
     return traffic(segment_id, start, end)
 
 
+def traffic_per_quarter(segment_id, year, quarter):
+    start = datetime.strptime(year, '%Y') + (quarter - 1) * timedelta(days=365 / 4)
+    end = start + timedelta(days=365 / 4)
+    return traffic(segment_id, start, end)
+
+
+def traffic_last_years(segment_id, years):
+    year = datetime.now().year
+    for i in range(years):
+        for q in range(1, 5):
+            yield year - i, q
+
+
 if __name__ == '__main__':
     segments = all_segments()
     with open(os.path.join('data', 'segments.json'), 'w') as f:
         json.dump(segments, f, indent=2)
-    now = datetime.now()
-    current_week = now - timedelta(days=now.weekday())
-    w = os.path.join('data', current_week.strftime('%Y-%m-%d'))
+
     for segment in segments['features']:
         s_id = segment['properties']['oidn']
-        os.makedirs(w, exist_ok=True)
-        filepath = os.path.join(w, f"{s_id}.json")
-        if os.path.exists(filepath):
-            continue
-        else:
-            data = traffic_last_3_months(s_id)
-            print(filepath, len(data))
-            if data['message'] == "Limit Exceeded":
-                print("Limit Exceeded")
-                break
-            elif data['message'] == "ok":
-                with open(filepath, 'w') as f:
+        for year, quarter in traffic_last_years(s_id, 10):
+            jsonpath = os.path.join('data', f'{s_id}_{year}_{quarter}.json')
+            if os.path.exists(jsonpath):
+                print("Already exists", jsonpath)
+                continue
+            data = traffic_per_quarter(s_id, str(year), quarter)
+            print(year, quarter, data['message'], f'{s_id}_{year}_{quarter}.json', len(data), ' bytes')
+            if data['message'] == "ok":
+                with open(jsonpath, 'w') as f:
                     json.dump(data, f, indent=2)
             else:
-                print("Unknown error", data)
-                break
+                print("Error", data)
             time.sleep(3)
